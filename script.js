c553d8610cd0bfaa78fd9714695c16b24dbcff90
Astroiwnl <79814411+Astroiwnl@users.noreply.github.com>
2026-01-31 16:41:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,13 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Idc youre doing as a i want"
-    "I mean it"
-"Stopp trying"
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
 ];
 
 let messageIndex = 0;
